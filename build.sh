29b66bc827e58a35e191f22a9a4f2cac81b7f69c
Rafael Folco <rafaelfolco@users.noreply.github.com>
2021-11-18 18:41:04 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,12 @@ cd $(dirname "${BASH_SOURCE[0]}")
 #update and init only libmoon, libmoons build.sh will do the rest recursivly
 git submodule update --init
 
-pushd libmoon/deps/dpdk/config
+pwd
+find . -type d
+cd libmoon/deps/dpdk/config
 curl -O https://raw.githubusercontent.com/rafaelfolco/dpdk/master/config/common_linuxapp
-popd
+cd -
+exit 0
 
 (
 cd libmoon
